c3ee0c17d560761507d4847d0e503a1d29cc3083
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2024-03-20 18:02:43 -0400

    sort

--- src/models/mesh.cpp
+++ src/models/mesh.cpp
@@ -18,12 +18,20 @@ void Mesh::draw(vk::CommandBuffer commandBuffer)
 {
     std::vector<Buffer*> buffers;
     // TODO: need to bind in right order
+
+    // sort vertex buffers by type
+    std::sort(vertexBuffers.begin(), vertexBuffers.end(), [](const auto &a, const auto &b) {
+        return a.second < b.second;
+    });
+
     buffers.reserve(vertexBuffers.size());
     for (auto &vb : vertexBuffers)
     {
         buffers.push_back(vb.first.get());
     }
     bind(commandBuffer, buffers);
+
     indexBuffer->bind(commandBuffer);
-    commandBuffer.drawIndexed(indexBuffer->info.size / sizeof(uint16_t), 1, 0, 0, 0);
+    int count = indexBuffer->info.size / sizeof(uint16_t);
+    commandBuffer.drawIndexed(count, 1, 0, 0, 0);
 }
