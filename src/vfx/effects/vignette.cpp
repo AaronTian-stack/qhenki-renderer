2397bee96af68940cfb61b2676e4435b1db30ffe
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2024-05-26 23:24:20 -0700

    better clamping

--- src/vfx/effects/vignette.cpp
+++ src/vfx/effects/vignette.cpp
@@ -13,11 +13,17 @@ Vignette::Vignette(vk::Device device, const char *shaderPath, PipelineBuilder &p
 
 void Vignette::renderMenu()
 {
-    ImGui::SliderFloat("Intensity", &data.intensity, 0.0f, 2.f);
-    ImGui::SliderFloat("Outer Ring", &data.vignetteX, 0.0f, 1.f);
-    ImGui::SliderFloat("Inner Falloff", &data.vignetteY, 0.0f, 1.f);
-    if (data.vignetteX <= data.vignetteY)
-        data.vignetteX = data.vignetteY + 0.001f;
+    ImGui::SliderFloat("Intensity", &data.intensity, 0.0f, 2.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+    if (ImGui::SliderFloat("Outer Ring", &data.vignetteX, 0.0f, 1.f, "%.3f", ImGuiSliderFlags_AlwaysClamp))
+    {
+        if (data.vignetteX <= data.vignetteY)
+            data.vignetteY = data.vignetteX - 0.001f;
+    }
+    if (ImGui::SliderFloat("Inner Falloff", &data.vignetteY, 0.0f, 1.f, "%.3f", ImGuiSliderFlags_AlwaysClamp))
+    {
+        if (data.vignetteY >= data.vignetteX)
+            data.vignetteX = data.vignetteY + 0.001f;
+    }
     ImGui::DragFloat("Saturation", &data.saturation, 0.01f, 10.f);
     ImGui::DragFloat("Saturation Mul", &data.saturationMul, 0.01f, 10.f);
 }
